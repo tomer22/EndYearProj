789a1c27c6cb0bbd4fe5d8fa77ed8cc3ce970a11
ChavaScript1 <98165018+ChavaScript1@users.noreply.github.com>
2023-06-17 18:31:49 -0400

    screen1 revision
    fully implemented background animations, polished the text and added fonts. Fixed prior alignment issues as well

--- screen1.dart
+++ screen1.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
 //import other files or 'pages' from here
 
 class HomePage extends StatefulWidget {
@@ -13,16 +16,55 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) =>
-      Scaffold(
-          body: Container(
-              width: double.infinity,
-              height: double.infinity,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("assets/atlasbackground.jpg"), //will be replaced with RIVE file once polished
-                    fit: BoxFit.cover
-                ),
-              )
-          )
+       Scaffold(
+        body: Stack(
+          children: [
+            RiveAnimation.asset("assets/RiveAssets/ani3.riv"),
+            Positioned.fill(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                child: SizedBox(),
+              ),
+            ),
+            const SafeArea(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        width: 300,
+                        child: Column(
+                          children: [
+                            Text(
+                              "Welcome to Atlas",
+                              style: TextStyle(
+                                fontSize: 100,
+                                fontFamily: "BebasNeue",
+                                height: 1.2,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Text(
+                              "get started now!",
+                              style: TextStyle(
+                                fontSize: 40,
+                                fontFamily: "BebasNeue",
+                                height: 1.2,
+                                color: Colors.white24,
+                              ),
+                            )
+
+                          ],
+                        )
+                      )
+
+                    ],
+       ),
+                )
+            )
+          ],
+        ),
       );
+
+
 }
