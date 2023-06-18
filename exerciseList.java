8f824b54777282d5ab954e33f7d0d8aeb9755e91
Tomer Inbar <tomerinbar66@gmail.com>
2023-06-18 12:58:39 -0400

    Created objects for each exercise

---
+++ exerciseList.java
@@ -0,0 +1,36 @@
+public class exerciseList {
+    Exercise benchPress = new Exercise("Bench press", "push", "chest");
+    Exercise dbInclineBench = new Exercise("DB Incline bench", "push", "chest");
+    Exercise pecDeck = new Exercise("Pec deck", "push", "chest");
+    Exercise shoulderPress = new Exercise("Shoulder press", "push", "shoulders");
+    Exercise dbLateralRaises = new Exercise("DB lateral raises", "push", "shoulders");
+    Exercise ropeTricepPushDown = new Exercise("Rope tricep push down", "push", "triceps");
+    Exercise skullCrushers = new Exercise("Skull Crushers", "push", "triceps");
+    Exercise latPullDown = new Exercise("Lat pull down", "pull", "back");
+    Exercise machineCableRow = new Exercise("Machine cable row", "pull", "back");
+    Exercise pullUp = new Exercise("Pull up", "pull", "back");
+    Exercise reversePecDeck = new Exercise("Reverse pec deck", "pull", "back");
+    Exercise barBicepCurl = new Exercise("Bar Bicep curl", "pull", "biceps");
+    Exercise dbHammerCurls = new Exercise("DB hammer curls", "pull", "biceps");
+    Exercise backSquat = new Exercise("Back Squat", "legs", "quadriceps");
+    Exercise legPress = new Exercise("Leg press", "legs", "quadriceps");
+    Exercise seatedHamstringCurls = new Exercise("Seated Hamstring curls", "legs", "hamstrings");
+    Exercise seatedLegExtensions = new Exercise("Seated leg extensions", "legs", "quadriceps");
+    Exercise seatedCalfRaises = new Exercise("Seated calf raises", "legs", "calves");
+    Exercise deadlift = new Exercise("Deadlift", "legs", "hamstrings");
+    Exercise lunges = new Exercise("Lunges", "legs", "quadriceps");
+    Exercise cableBicepCurl = new Exercise("Cable bicep curl", "pull", "biceps");
+    Exercise standingCalfRaises = new Exercise("Standing calf raises", "legs", "calves");
+    Exercise cableChestFlyes = new Exercise("Cable chest flyes", "push", "chest");
+    Exercise tricepDips = new Exercise("Tricep dips", "push", "triceps");
+    Exercise lyingLegCurl = new Exercise("Lying leg curl", "legs", "hamstrings");
+    Exercise chestPress = new Exercise("Chest press", "push", "chest");
+    Exercise legAbductor = new Exercise("Leg abductor", "legs", "abductors");
+    Exercise legAdductor = new Exercise("Leg adductor", "legs", "adductors");
+    Exercise barbellRows = new Exercise("Barbell rows", "pull", "back");
+    Exercise dumbbellFlyes = new Exercise("Dumbbell flyes", "push", "chest");
+    Exercise cableHammerCurls = new Exercise("Cable hammer curls", "pull", "biceps");
+    Exercise tricepKickbacks = new Exercise("Tricep kickbacks", "push", "triceps");
+    Exercise declineBenchPress = new Exercise("Decline bench press", "push", "chest");
+    Exercise cableReverseFlyes = new Exercise("Cable reverse flyes", "pull", "back");
+}
